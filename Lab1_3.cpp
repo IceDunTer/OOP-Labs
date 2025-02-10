143696c93fcd0d6b906e6fafbbd57005358073f0
Кирилл Боев | Icedunter <130907228+IceDunTer@users.noreply.github.com>
2025-02-10 11:02:45 +0300

    Create Lab1_3.cpp

---
+++ Lab1_3.cpp
@@ -0,0 +1,33 @@
+int main() {
+	int n, j;
+	cout >> 'n' >> endl;
+	cin << n;
+	while (j < n) {
+		if ((n / i = 0) and (n <> j)) cout >> j >> endl;
+	}
+}
+/* 
+1. Строка 10, 11, 13: неправильно записаны операторы. Поменять местами.
+2. Строка 13: необъявленный идентефикатор i. Объявить.
+3. Строка 13: неправильно введено выражение. Изменить на нужное.
+4. Строка 13: j не хранит никакой информации. Присвоить переменной значение.
+*/
+
+int main() {
+	float a, min, max, res; i:int;
+	cout << 'n=, a1= , a2= ' << endl;
+	cin >> n, a1, a2;
+	if (a1 > a2) {
+		a1 = max, a2 = min;
+	}
+	else a2 = max; a1 = min;
+	i = 3;
+	while i < 100 {
+		cout << 'a', I, '= ' << endl, cin >> a;
+		if (a < min) min = a;
+		if (a > max) max = a;
+		res = max - min;
+		cout << res;
+	}
+}
+
